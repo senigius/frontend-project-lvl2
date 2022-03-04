22cd1a67e8cfe9064d8394eade3abae910a31f3b
Dmitry Utkin <senigius8@gmail.com>
2022-03-05 01:04:23 +0300

    fix

--- bin/searchForDiff.js
+++ bin/searchForDiff.js
@@ -1,31 +1,25 @@
 import _ from 'lodash';
 
 const searchForDiff = (obj1, obj2) => {
-  const buildNode = (key) => {
+  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
+  const sortedKeys = _.sortBy(keys);
+  return sortedKeys.map((key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
+    let result = { key, value: value1, type: 'unchanged' };
     if (!_.has(obj2, key)) {
-      return { key, value: value1, type: 'deleted' };
-    }
-    if (!_.has(obj1, key)) {
-      return { key, value: value2, type: 'added' };
-    }
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, children: searchForDiff(value1, value2), type: 'haveChildren' };
-    }
-    if (!_.isEqual(value1, value2)) {
-      return {
-        key,
-        oldValue: value1,
-        newValue: value2,
-        type: 'changed',
+      result = { key, value: value1, type: 'deleted' };
+    } else if (!_.has(obj1, key)) {
+      result = { key, value: value2, type: 'added' };
+    } else if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      result = { key, children: searchForDiff(value1, value2), type: 'haveChildren' };
+    } else if (!_.isEqual(value1, value2)) {
+      result = {
+        key, oldValue: value1, newValue: value2, type: 'changed',
       };
     }
-    return { key, value: value1, type: 'unchanged' };
-  };
-
-  const keys = _.union(Object.keys(obj1), Object.keys(obj2));
-  return _.sortBy(keys).map(buildNode);
+    return result;
+  });
 };
 
 export default searchForDiff;
