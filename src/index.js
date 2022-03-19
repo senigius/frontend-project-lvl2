f0ef9fad2ea6b9ed1c72b3e7fce247f9d18c153c
Dmitry Utkin <senigius8@gmail.com>
2022-03-19 23:34:31 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import buildDiffTree from './buildDiffTree.js';
 import formatOutput from './formatters/index.js';
 import parseData from './parser.js';
 
-const getFixturePath = (normalPath) => path.resolve(process.cwd(), '__tests__/__fixtures__', normalPath);
+const getFixturePath = (filepath) => path.resolve(process.cwd(), '__tests__/__fixtures__', filepath);
 
-const getData = (filepath) => {
-  const absPath = path.isAbsolute(filepath) ? filepath : getFixturePath(filepath);
-  const data = fs.readFileSync(absPath, 'utf8');
-  const fileType = path.extname(filepath).slice(1);
-  return parseData(fileType, data);
-};
+const getAbsPath = (filepath) => (path.isAbsolute(filepath) ? filepath : getFixturePath(filepath));
+
+const getFileType = (filepath) => path.extname(filepath).slice(1);
+
+const getData = (filepath) => fs.readFileSync(filepath, 'utf8');
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = getData(filepath1);
-  const data2 = getData(filepath2);
+  const absPath1 = getAbsPath(filepath1);
+  const absPath2 = getAbsPath(filepath2);
+  const typeOfFile1 = getFileType(filepath1);
+  const typeOfFile2 = getFileType(filepath2);
+  const data1 = parseData(getData(absPath1), typeOfFile1);
+  const data2 = parseData(getData(absPath2), typeOfFile2);
   const result = buildDiffTree(data1, data2);
   return formatOutput(result, formatName);
 };
--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,6 @@
 import yaml from 'js-yaml';
 
-export default (type, data) => {
+export default (data, type) => {
   if (type === 'json') return JSON.parse(data);
   if (type === 'yml') return yaml.load(data);
   if (type === 'yaml') return yaml.load(data);
